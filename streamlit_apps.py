175d7078d666617020132ac1935e43dc145be96d
Danish Shahid <danishshahid19@gmail.com>
2022-03-01 14:22:21 +0500

    Update streamlit_apps.py

--- streamlit_apps.py
+++ streamlit_apps.py
@@ -8,9 +8,7 @@ def main():
 
 	if choice == "Image":
 		st.subheader("Image")
-
-       elif choice == "Dataset":
+	elif choice == "Dataset":
 		st.subheader("Dataset")
-
-       elif choice == "DocumentFiles":
+	elif choice == "DocumentFiles":
 		st.subheader("DocumentFiles")
